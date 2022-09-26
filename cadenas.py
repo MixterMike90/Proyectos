2562375b6948ffab9f5d43a48870b5e72bab1b92
MixterMike90 <fockin_awesome@hotmail.com>
2022-09-26 00:46:29 -0500

    saludos este es el codigo

---
+++ cadenas.py
@@ -0,0 +1,52 @@
+# texto_variado = 'Palabra, 123 * #'
+# print(type(texto_variado)) 
+
+# Podemos utilizar comillas triples para que el texto se muestre como la cadena que hemos escrito
+# print(""" 
+# Funcionamiento de \
+# Programa : (opciones)
+#     1.- Para acceder a Opciones
+#         2.- Salir
+
+
+
+# """)
+####################################
+# Subscripting e indexado
+
+
+
+#texto = 'Python'
+# print(texto[0])
+# print(texto[5])
+# print(texto[-1])
+# print(texto[-6]) 
+
+#print(texto[6]) #Error no se puede acceder a una posicion que no existe.
+#print(texto[-7]) # Error no se puede acceder a una posicion que no existe.
+
+#letra = texto[0]
+#print(letra)
+
+#texto[0]= 'p' Error no podemos modificar una cadena
+
+#letra = 'p'
+#print(letra)
+
+# texto_compuesto = letra + texto[1] #concatenación
+# print(texto_compuesto)
+
+#########################################
+
+# Slicing o Substringing
+texto = 'Python'
+#print(texto[0:3])
+#print(texto[0:-3])
+# print(texto[2:])
+# print(texto[:3])
+
+# print(texto[-3::-1])
+#print(texto[::-1])
+#print(texto[1:50])
+print(texto[2:2])
+
