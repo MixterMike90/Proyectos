00d11fc1480c5bef72e65133f331577f1aaed7ec
MixterMike90 <fockin_awesome@hotmail.com>
2022-10-06 01:41:09 -0500

    Autor:Miguel Angel Vazquez Collazo

---
+++ Condicionales_M2.py
@@ -0,0 +1,77 @@
+#Ejemplo del Tema 1 del Modulo 2
+# animal=input("Dime un animal: ")
+# if animal=="perro":
+#     print("-guau")
+# print("Conozco pocos animales.")
+
+#Ejemplo 2
+# num=int(input("Ingrese un entero: "))
+# if num<0:
+#     num=-num
+# print("Su valor absoluto es: ", num)
+
+#Ejemplo 3 Estructura if -else
+# animal=(input("Dime un animal: "))
+# if animal=="perro":
+#     print("-guau")
+# else:
+#     print("No conozco su sonido.")
+    
+# print("Conozco pocos animales")
+
+#Ejemplo 4 Estructura if-else-elif
+
+# animal=(input("Dime un animal: "))
+# if animal=="perro":
+#      print("-guau")
+# elif animal=="gato":
+#     print("-Miau.")
+# elif animal=="vaca":
+#     print("-Muu.")
+# else:
+#     print("No conozco su sonido.")
+
+# print("Conozco pocos animales.")
+
+#Ejemplo 5 con condicional While
+
+# print("Impares menores a 10")
+# x=1
+# while x<=10:
+#     print(x)
+#     x+=2 
+
+# #######
+
+# Ejemplo 6
+
+# factorial=5
+# contador = factorial-1
+# while contador>0:
+#     factorial*=contador
+#     contador-=1
+#     print(factorial)
+
+#Ejemplo con el condicionante for
+
+for i in 1,2,3:
+    print(i)
+
+#Ejemplo 2 con el condicionante for
+
+# for i in range(5):
+#     print (i)
+
+#Ejemplo 3 con el condicionante for
+
+for i in "Hola Mundo":
+    if i=="M":
+        pass
+    else:
+        print(i,end="")
+    
+
+
+
+
+
---
+++ operadores.py
@@ -0,0 +1,42 @@
+#operadores aritméticos
+#print(2 + 2) #suma
+# print(4 - 2) #resta
+# print(3 * 7) #multiplicación 
+# print(15 / 2) #división
+# print(11 % 4)  #módulo
+# print(11 // 4) #división de piso
+# print(2 ** 3)  #potenciación
+
+# #operadores en cadenas
+# print('Hola'+'Mundo') #concatenación
+# print ('Hola' * 3) #repetición
+# print('Hola' + 'Mundo' * 3) #concatenación y multiplicación
+
+#Operadores de comparación
+# print ('Hola'== 'hola') # Igual que
+# print ('Hola' != 'hola') # Distinto que
+# print (3 > 11) #Mayor que , Menor que
+# print (11 >= 10) #Mayor o Igual que
+# print (10 <= 10) #Menor o Igual que  
+
+# #Operadores de Existencia
+# print('ola' in 'Hola') #De existencia
+# print ('ola' not in 'Hola') #De inexistencia
+
+#Operadores Booleanos
+# print(True or False) # or verifica que se cumplan solo una de las dos condiciones
+# print(True and False)#  and verifica que se cumplan las dos condiciones
+
+#Operadores de asignación
+# x = 1 # Operador de asignación estándar
+# x += 2 # Operador de asignación Suma
+# x *=3 # Operador de asignación multiplicación
+# x %= 4 # Operador de asignación Módulo
+# print(x)
+
+x = 6 * 5 + 8 / 2 ** 4
+print(x)
+
+
+
+
