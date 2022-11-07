a0c0b6d0592441da7dd6778b6a0d7ef705dd27a3
MixterMike90 <fockin_awesome@hotmail.com>
2022-11-06 23:10:00 -0600

    Proyecto_Modulo2

--- MiguelAngel_VazquezCollazo_proyectoM2.py
+++ MiguelAngel_VazquezCollazo_proyectoM2.py
@@ -10,13 +10,20 @@
 # Tiene N letras ((siendo N el número de letras de la palabra))
 #=========================================================================================
 
+# Se le pide al usuario introduzca la frase que guste en el programa
 frase=input("Introduce una frase: ")
+#Se va a evaluar el contenido de las letras que conforma la frase introducida por el usuario
+#Primero se evaluara si cumple con la primera condicion solicitada 
 if len(frase)>=4 and len(frase)<9 :
      print("La palabra es correcta")
+#En caso que no cumpla la primera condicion se pasara a la siguiente para saber si cumple con la 
+#Segunda condicion, dando salida a pantalla como lo solicita los puntos requeridos
 if len(frase)<4 :
      letras=0
      letras=len(frase)
      print("Hacen falta letras. Solo tiene",letras,"letras")
+#En caso de que no cumpla con las otras condiciones se determinara que cumpla con la tercera 
+#condicion,dando salida a pantalla como lo solicita los puntos requeridos
 if len(frase)>8:
      sletras=0
      sletras=len(frase)
@@ -32,10 +39,15 @@ if len(frase)>8:
 # Ingrese Y: -5
 # El punto se encuentra en el cuadrante IV.
 
+#Primero Se solicitan los datos , en este caso el valor de X y el de Y
 print("Introduzca las coordenadas, para saber en que cuadrante se encuentra el punto")
 coordenadax=float(input("Introduzca el Valor de X: "))
 coordenaday=float(input("Introduzca el Valor de Y: "))
 
+#Se realizaron los criterios de evaluacion para que el valor dado por el usuario del punto a ubicar
+#Nos indique el programa que cuadrante se ubica el punto 
+#O en que eje se ubica o si todavia esta en el origen, mostrando en que cuadrante esta por medio de 
+#Un mensaje
 if coordenadax>0 and coordenaday>0 :
     print("El punto se encuentra en el cuadrante I ")
 elif coordenadax<0 and coordenaday>0 :
